08e46fba740f06fd13bf6225144f789ba63b0fbb
Jeffrey Jose <jefjosecool@gmail.com>
2022-10-20 14:40:41 -0400

    I think I fixed it

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace SelectionStatementExercise
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Please input a number to guess");
+            var userInput = int.Parse(Console.ReadLine());
+
+            var answer = Guess(favNumber, userInput);
+            Console.WriteLine(answer);
+
+            //Create a methos which returns what the users guess and see if it correct
         }
+            static string Guess(int favNumber,int userinput)
+            {
+
+
+                if (favNumber > userinput)
+                {
+
+                    return ("To low");
+
+                }
+                else if (favNumber < userinput)
+                {
+                    return ("To High");
+                }
+
+                else
+                {
+
+                    return ("Youu Guessed itt");
+                }
+
+
+
+            }
     }
 }
